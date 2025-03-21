8806138ff903f3c4f6187f172b3f449ed9e20ba0
Geniot <jude.guehl@efrei.net>
2025-03-21 09:56:44 +0100

    Change all automata files to .txt and put them in the same folder
    fix potential recursivity bug in doStuff with automata

--- menu.py
+++ menu.py
@@ -6,12 +6,8 @@ import Complete as O
 import Complementary as inverse
 
 def getFIleFromInput(val):
-    if val<23 and val>0:
-        filepath="Automaton"+str(val)
-        return  filepath
-    
-    if val<45 and val>0:
-        filepath="Automaton 23 -44/Automaton"+str(val)+".txt"
+    if val>=1 and val<=44:
+        filepath="Automaton/Automaton"+str(val)+".txt"
         return  filepath
     print("Invalid automaton number")
     return 0
@@ -23,38 +19,39 @@ def Menu():
     filepath=getFIleFromInput(val)
     if filepath!=0:
         automata=r.readFileToDictionary(filepath)
-        print(automata)
+        automata.display()
         DoStuffWithAutomata(automata)
 
 
 def DoStuffWithAutomata(automata):
-    choice=int(input("What would you like to do : \n1 : Enter a new automaton\n2 : Determinize the automaton\n3 : Complete the automaton\n4 : Standardize the automaton\n5 : exit :\n")
-    )
-    if choice==1:
-        Menu()
-    if choice==2:
-        determinizedAutomata=d.DeterminizeAutomata(automata)
-        while d.is_deterministic(determinizedAutomata) != True:
-            determinizedAutomata=d.DeterminizeAutomata
-        print("Here is the determinized automaton : \n")
-        print(determinizedAutomata)
-        DoStuffWithAutomata(automata)
+    choice=int(input("What would you like to do : \n1 : Enter a new automaton\n2 : Determinize the automaton\n3 : Complete the automaton\n4 : Standardize the automaton\n5 : exit :\n"))
 
-    if choice==3:
-        complete_automata=O.CompleteAutomata(automata)
-        print("Here is the complete automata : \n")
-        print(complete_automata)
-        DoStuffWithAutomata(automata)
-    
-    if choice==4:
-        if s.isStandard(automata):
-            print("The automata is already standard\n")
-        else:
-            standardizedAutomata=s.Standardize(automata)
-            print(standardizedAutomata)
-        DoStuffWithAutomata(automata)
+    while not(choice<1 or choice>5):
+        if choice==1:
+            val=int(input("Enter the number of the automaton : \n"))
+            filepath=getFIleFromInput(val)
+            if filepath!=0:
+                automata=r.readFileToDictionary(filepath)
+                automata.display()
+        if choice==2:
+            determinizedAutomata=d.DeterminizeAutomata(automata)
+            while d.is_deterministic(determinizedAutomata) != True:
+                determinizedAutomata=d.DeterminizeAutomata
+            print("Here is the determinized automaton : \n")
+            determinizedAutomata.display()
 
-    if choice==5:
-        exit()
+        if choice==3:
+            complete_automata=O.CompleteAutomata(automata)
+            print("Here is the complete automata : \n")
+            complete_automata.display()
+
+        if choice==4:
+            if s.isStandard(automata):
+                print("The automata is already standard\n")
+            else:
+                standardizedAutomata=s.Standardize(automata)
+                standardizedAutomata.display()
+        choice=int(input("What would you like to do : \n1 : Enter a new automaton\n2 : Determinize the automaton\n3 : Complete the automaton\n4 : Standardize the automaton\n5 : exit :\n"))
+
             
     
